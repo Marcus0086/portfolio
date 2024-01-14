52a33707ed73059308180ebfba267e8f19465095
Marcus0086 <guptamarcus42@gmail.com>
2024-01-15 01:36:41 +0530

    Update next.config.js and tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    skills: process.env.skills,
+    userConfig: process.env.userConfig,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
